b8a10f1f8637002116468a699e919f835844c80c
Singlem <Singlem@gmail.com>
2010-10-20 10:17:42 +0200

    Added support for quest 12570

--- scripts/northrend/sholazar_basin.cpp
+++ scripts/northrend/sholazar_basin.cpp
@@ -26,6 +26,7 @@ npc_vekjik
 EndContentData */
 
 #include "precompiled.h"
+#include "escort_ai.h"
 
 /*######
 ## npc_vekjik
@@ -81,6 +82,88 @@ bool GossipSelect_npc_vekjik(Player* pPlayer, Creature* pCreature, uint32 uiSend
     return true;
 }
 
+/*######
+## npc_injured_oracle
+######*/
+
+enum InjuredOracle
+{
+	SAY_ESCORT_FINISHED                 = -1571000,
+	SAY_ESCORT_READY                    = -1571001,
+	SAY_ESCORT_START                    = -1571002,
+
+	SPELL_QUEST_READY                   = 53807,
+	SPELL_ESCORT_START                  = 51341,
+
+	QUEST_FORTUNATE_MISUNDERSTANDINGS   = 12570
+};
+
+#define GOSSIP_INJURED_ORACLE "I am ready to travel to your village now."
+
+struct MANGOS_DLL_DECL npc_injured_oracleAI : public npc_escortAI
+{
+	npc_injured_oracleAI(Creature* pCreature) : npc_escortAI(pCreature) { Reset(); }
+
+	void Reset(){}
+
+	void WaypointReached(uint32 uiPointId)
+	{
+		switch(uiPointId)
+		{
+		case 24:
+			DoScriptText(SAY_ESCORT_FINISHED, m_creature);
+				if (Player* pPlayer = GetPlayerForEscort())
+					pPlayer->AreaExploredOrEventHappens(QUEST_FORTUNATE_MISUNDERSTANDINGS);
+				break;
+		}
+	}
+};
+
+CreatureAI* GetAI_npc_injured_oracle(Creature* pCreature)
+{
+	return new npc_injured_oracleAI(pCreature);
+}
+
+bool GossipHello_npc_injured_oracle(Player* pPlayer, Creature* pCreature)
+{
+	if (pCreature->isQuestGiver())
+		pPlayer->PrepareQuestMenu(pCreature->GetGUID());
+	
+	if (pPlayer->GetQuestStatus(QUEST_FORTUNATE_MISUNDERSTANDINGS) == QUEST_STATUS_INCOMPLETE)
+		pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_INJURED_ORACLE, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+
+	pPlayer->SEND_GOSSIP_MENU(pPlayer->GetGossipTextId(pCreature), pCreature->GetGUID());
+	return true;
+}
+
+bool GossipSelect_npc_injured_oracle(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
+{
+	if (uiAction == GOSSIP_ACTION_INFO_DEF+1)
+	{
+		pPlayer->CLOSE_GOSSIP_MENU();
+			
+		if (npc_injured_oracleAI* pEscortAI = dynamic_cast<npc_injured_oracleAI*>(pCreature->AI()))
+		{
+			pCreature->RemoveAllAuras();
+			pEscortAI->Start(true, pPlayer->GetGUID());
+			pCreature->setFaction(FACTION_ESCORT_N_NEUTRAL_ACTIVE);
+			DoScriptText(SAY_ESCORT_START, pCreature);
+			pPlayer->CastSpell(pCreature, SPELL_ESCORT_START, true);
+		}
+	}
+	return true;
+}
+
+bool QuestAccept_fortunate_misunderstandings(Player* pPlayer, Creature* pCreature, const Quest* pQuest)
+{
+	if (pQuest->GetQuestId() == QUEST_FORTUNATE_MISUNDERSTANDINGS)
+	{
+		pCreature->CastSpell(pCreature, SPELL_QUEST_READY, true);
+		DoScriptText(SAY_ESCORT_READY, pCreature);
+	}
+	return true;
+}
+
 void AddSC_sholazar_basin()
 {
     Script *newscript;
@@ -90,4 +173,12 @@ void AddSC_sholazar_basin()
     newscript->pGossipHello = &GossipHello_npc_vekjik;
     newscript->pGossipSelect = &GossipSelect_npc_vekjik;
     newscript->RegisterSelf();
+
+    newscript = new Script;
+	newscript->Name = "npc_injured_oracle";
+	newscript->GetAI = &GetAI_npc_injured_oracle;
+	newscript->pQuestAccept = &QuestAccept_fortunate_misunderstandings;
+	newscript->pGossipHello = &GossipHello_npc_injured_oracle;
+	newscript->pGossipSelect = &GossipSelect_npc_injured_oracle;
+	newscript->RegisterSelf();
 }
